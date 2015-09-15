2ce853f8b686f091fb89d7963e70f732cb8c3942
Nelson Menezes <nelson.menezes@bouvet.no>
2015-09-15 10:53:17 +0200

    Change references from "element" to "container" queries

--- _dist/index.php
+++ _dist/index.php
@@ -15,7 +15,7 @@
       echo "</style>";
     } else {
       echo '<link rel="stylesheet" href="/_css/all.css">';
-    } ?> 
+    } ?>
 
     <noscript>
       <link rel="stylesheet" href="/_css/all.css">
@@ -42,42 +42,42 @@
 <div class="feat">
   <div class="head">
     <span>
-      <h3 class="feat-hed">Element Queries</h3>
+      <h3 class="feat-hed">Container Queries</h3>
       <p class="status">Initial Planning</p>
     </span>
   </div>
   <div class="body">
-    
+
     <div class="col-b">
       <h4 class="impl-hed">Impl<span class="abbr">ementation</span> Status</h3>
       <div class="support">
-        
+
           <img src="_img/icon-chrome.png" alt="chrome" >
-        
+
           <img src="_img/icon-firefox.png" alt="firefox" >
-        
+
           <img src="_img/icon-opera.png" alt="opera" >
-        
+
           <img src="_img/icon-safari.png" alt="safari" >
-        
+
           <img src="_img/icon-ie.png" alt="ie" >
-        
+
       </div>
     </div>
     <div class="col-a">
-      <p><p>Media queries allow authors to vary the layout of their page based on the dimensions of the viewport. <em>Element</em> queries will allow developers to vary the layout within specific elements on a page (and their children) based on the dimensions of the parent elements themselves, allowing for much more modular approaches to layout.</p><p>Work on element queries has just begun; we are still collecting use cases and working to understand technical constraints. Please contribute!</p></p>
+      <p><p>Media queries allow authors to vary the layout of their page based on the dimensions of the viewport. <em>Container</em> queries will allow developers to vary the layout within specific elements on a page (and their children) based on the dimensions of parent containers, allowing for much more modular approaches to layout.</p><p>Work on container queries has just begun; we are still collecting use cases and working to understand technical constraints. Please contribute!</p></p>
 
-      
+
 
       <h4 class="subhed">Documentation</h3>
       <dl class="docs">
-        
+
         <dt >Repo</dt>
-        <dd ><a href="https://github.com/ResponsiveImagesCG/eq-usecases">https://github.com/ResponsiveImagesCG/eq-usecases</a></dd>
-        
+        <dd ><a href="https://github.com/ResponsiveImagesCG/cq-usecases">https://github.com/ResponsiveImagesCG/cq-usecases</a></dd>
+
         <dt >Use Cases</dt>
-        <dd ><a href="https://responsiveimagescg.github.io/eq-usecases/">https://responsiveimagescg.github.io/eq-usecases/</a></dd>
-        
+        <dd ><a href="https://responsiveimagescg.github.io/cq-usecases/">https://responsiveimagescg.github.io/cq-usecases/</a></dd>
+
       </dl>
 
     </div>
@@ -92,27 +92,27 @@
     </span>
   </div>
   <div class="body">
-    
+
     <div class="col-b">
       <h4 class="impl-hed">Impl<span class="abbr">ementation</span> Status</h3>
       <div class="support">
-        
+
           <img src="_img/icon-chrome.png" alt="chrome" class="full-support">
-        
+
           <img src="_img/icon-firefox.png" alt="firefox" >
-        
+
           <img src="_img/icon-opera.png" alt="opera" class="full-support">
-        
+
           <img src="_img/icon-safari.png" alt="safari" >
-        
+
           <img src="_img/icon-ie.png" alt="ie" >
-        
+
       </div>
     </div>
     <div class="col-a">
       <p><p>The <code>picture</code> element contains any number of child <code>source</code> elements and one <code>img</code>.</p><p>When this pattern is used with the <code>media</code> attribute on <code>source</code> elements, it allows developers to mandate that user agents must load specific  sources when certain media conditions are met. This allows developers to specify alternate aspect ratios to align with layoutchanges, or alternate framing of image content for optimally highlighting the subject of the image at different sizes.</p><p>When <code>picture</code> and <code>source</code> are used with the <code>type</code> attribute, authors may supply the same image in alternate formats to be requested only by browsers that support said format.</p></p>
 
-      
+
       <h4 class="subhed">Usage</h3>
       <div class="usage">
         <code><pre>&lt;picture&gt;
@@ -120,20 +120,20 @@
   &lt;img src=&quot;small.jpg&quot; alt=&quot;…&quot;&gt;
 &lt;picture&gt;</pre></code>
       </div>
-      
+
 
       <h4 class="subhed">Documentation</h3>
       <dl class="docs">
-        
+
         <dt >Repo</dt>
         <dd ><a href="https://github.com/ResponsiveImagesCG/picture-element">https://github.com/ResponsiveImagesCG/picture-element</a></dd>
-        
+
         <dt >Use Cases</dt>
         <dd ><a href="http://usecases.responsiveimages.org/">http://usecases.responsiveimages.org/</a></dd>
-        
+
         <dt class="published">Specification</dt>
         <dd class="published"><a href="https://html.spec.whatwg.org/multipage/embedded-content.html#introduction-3:art-direction-3">https://html.spec.whatwg.org/multipage/embedded-content.html#introduction-3:art-direction-3</a></dd>
-        
+
       </dl>
 
     </div>
@@ -148,42 +148,42 @@
     </span>
   </div>
   <div class="body">
-    
+
     <div class="col-b">
       <h4 class="impl-hed">Impl<span class="abbr">ementation</span> Status</h3>
       <div class="support">
-        
+
           <img src="_img/icon-chrome.png" alt="chrome" class="full-support">
-        
+
           <img src="_img/icon-firefox.png" alt="firefox" >
-        
+
           <img src="_img/icon-opera.png" alt="opera" class="full-support">
-        
+
           <img src="_img/icon-safari.png" alt="safari" >
-        
+
           <img src="_img/icon-ie.png" alt="ie" >
-        
+
       </div>
     </div>
     <div class="col-a">
       <p><p><code>srcset</code>’s <code>w</code> descriptor and the <code>sizes</code> attribute allow developers to supply a range of alternate sources for images whose sizes vary within a responsive layout. Optionally working in conjunction with server-side resizing, these syntaxes allow for a fully automated “responsive images” solution.</p></p>
 
-      
+
       <h4 class="subhed">Usage</h3>
       <div class="usage">
         <code><pre>&lt;img src=&quot;small.jpg&quot; sizes=&quot;100vw&quot; srcset=&quot;med.jpg 800w, large.jpg 1400w&quot; alt=&quot;…&quot;&gt;</pre></code>
       </div>
-      
+
 
       <h4 class="subhed">Documentation</h3>
       <dl class="docs">
-        
+
         <dt >Use Cases</dt>
         <dd ><a href="http://usecases.responsiveimages.org/">http://usecases.responsiveimages.org/</a></dd>
-        
+
         <dt class="published">Specification</dt>
         <dd class="published"><a href="https://html.spec.whatwg.org/multipage/embedded-content.html#introduction-3:viewport-based-selection-2">https://html.spec.whatwg.org/multipage/embedded-content.html#introduction-3:viewport-based-selection-2</a></dd>
-        
+
       </dl>
 
     </div>
@@ -198,42 +198,42 @@
     </span>
   </div>
   <div class="body">
-    
+
     <div class="col-b">
       <h4 class="impl-hed">Impl<span class="abbr">ementation</span> Status</h3>
       <div class="support">
-        
+
           <img src="_img/icon-chrome.png" alt="chrome" class="full-support">
-        
+
           <img src="_img/icon-firefox.png" alt="firefox" >
-        
+
           <img src="_img/icon-opera.png" alt="opera" class="full-support">
-        
+
           <img src="_img/icon-safari.png" alt="safari" class="full-support">
-        
+
           <img src="_img/icon-ie.png" alt="ie" >
-        
+
       </div>
     </div>
     <div class="col-a">
       <p><p>The <code>srcset</code> attribute allows developers to supply user agents with alternate versions of an image, intended for display at different pixel densities.</p><p>In its most basic and well-supported form, srcset lets developers use <code>x</code> descriptors to specify the intended pixel-density of each source.</p></p>
 
-      
+
       <h4 class="subhed">Usage</h3>
       <div class="usage">
         <code><pre>&lt;img src=&quot;standard-definition.jpg&quot; srcset=&quot;high-definition.jpg 2x&quot;&gt;</pre></code>
       </div>
-      
+
 
       <h4 class="subhed">Documentation</h3>
       <dl class="docs">
-        
+
         <dt >Use Cases</dt>
         <dd ><a href="http://usecases.responsiveimages.org/">http://usecases.responsiveimages.org/</a></dd>
-        
+
         <dt class="published">Specification</dt>
         <dd class="published"><a href="https://html.spec.whatwg.org/multipage/embedded-content.html#introduction-3:device-pixel-ratio-2">https://html.spec.whatwg.org/multipage/embedded-content.html#introduction-3:device-pixel-ratio-2</a></dd>
-        
+
       </dl>
 
     </div>
